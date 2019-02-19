1ebc4977e9d6cf5f703531690c6881d9f911d920
Raymond-MacbookPro <ordinaryapple@gmail.com>
2019-02-18 18:39:09 -0800

    First Commit

---
+++ src/components/Error.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Error extends Component {
+
+  render() {
+    return(
+      <div>
+        <h1>No such page exists</h1>
+      </div>
+    );
+  }
+
+}
+
+export default Error;
