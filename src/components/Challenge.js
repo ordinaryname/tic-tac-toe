ead92670b74eda9564d394e8d630147125bee51c
Raymond-MacbookPro <ordinaryapple@gmail.com>
2019-03-24 22:47:43 -0700

    Working on challenges

--- src/components/Challenge.js
+++ src/components/Challenge.js
@@ -37,12 +37,17 @@ class Challenge extends Component {
       <div className="challenge">
         <Header/>
         <div className="myChallenges">
-          {this.challenges}
+        <h2 className="myChallengesTitle">My Challenges</h2>
+          {this.challenges()}
         </div>
         <div className="myFriends">
-          <input type="text" className="enterFriendName"/>
+          <h2 className="myFriendsTitle">My Friends</h2>
+          <input type="text" className="enterFriendName" placeholder="Find A Friend"/>
           <button className="findFriend">Search</button>
-          {this.friends}
+          {this.friends()}
+        </div>
+        <div className="footer">
+          <div className="footerText">&#169; <a href="https://raymondmutyaba.com/" className="copyrightText">Raymond Mutyaba</a> 2019</div>
         </div>
       </div>
     );
