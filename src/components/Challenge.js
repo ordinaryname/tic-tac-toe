dcdc28b2d34023acf51c596febb16fbb17985a15
Raymond-MacbookPro <ordinaryapple@gmail.com>
2019-03-26 22:13:42 -0700

    Fix bugs in user search

--- src/components/Challenge.js
+++ src/components/Challenge.js
@@ -8,6 +8,8 @@ class Challenge extends Component {
     super(props);
     this.state = {
       hasUsername:false,
+      userInput:'',
+      friends:[],
     };
   }
 
@@ -15,20 +17,26 @@ class Challenge extends Component {
 
   }
 
-  getUser = () => {
-    fetch('/users')
+  updateFindFriend = (event) => {
+    this.setState({userInput:event.target.value});
+  }
+
+  getUser = (event) => {
+    event.preventDefault();
+    fetch(`/users/${this.state.userInput}`)
     .then(results => {return results.json();})
+    .then(data => {
+      let friends = Object.values(data).map((friend) => {
+        console.log({friend});
+        return({friend}["friend"])
+      })
+      this.setState({friends: friends});
+    })
   }
 
   challenges = () => {
     return(
       <div className="challengesList"></div>
-    );
-  }
-
-  friends = () => {
-    return(
-      <div className="friendsList"></div>
     );
   }
 
@@ -42,9 +50,9 @@ class Challenge extends Component {
         </div>
         <div className="myFriends">
           <h2 className="myFriendsTitle">My Friends</h2>
-          <input type="text" className="enterFriendName" placeholder="Find A Friend"/>
-          <button className="findFriend">Search</button>
-          {this.friends()}
+          <input type="text" className="enterFriendName" placeholder="Find A Friend" onChange={this.updateFindFriend} onKeyUp={this.updateFindFriend}/>
+          <button className="findFriend" onClick={this.getUser}>Search</button>
+          <div className="friendsList">{this.state.friends}</div>
         </div>
         <div className="footer">
           <div className="footerText">&#169; <a href="https://raymondmutyaba.com/" className="copyrightText">Raymond Mutyaba</a> 2019</div>
